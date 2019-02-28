02723e4d226b18292d1d9c7898c3b7ab7c7821fe
Dimitar Dimitrov <daghostman.dd@gmail.com>
2019-02-28 22:38:49 +0200

    Return timer-like object to allow for delayed stopping of timers

--- src/EventLoop/Scheduler/SwooleScheduler.php
+++ src/EventLoop/Scheduler/SwooleScheduler.php
@@ -4,6 +4,7 @@ namespace Onion\Framework\EventLoop\Scheduler;
 use Closure;
 use Onion\Framework\EventLoop\Interfaces\SchedulerInterface;
 use Onion\Framework\EventLoop\Task\Descriptor;
+use Onion\Framework\EventLoop\Task\Timer;
 
 class SwooleScheduler implements SchedulerInterface
 {
@@ -23,7 +24,20 @@ class SwooleScheduler implements SchedulerInterface
         if ($interval <= 0) {
             $interval = 1;
         }
-        swoole_timer_tick($interval, $callback);
+        $timer = swoole_timer_tick($interval, $callback);
+
+        return new class($timer) extends Timer {
+            private $timer;
+
+            public function __construct($timer) {
+                $this->timer = $timer;
+            }
+            public function run() {}
+            public function stop() {
+                swoole_timer_clear($this->timer);
+                parent::stop();
+            }
+        };
     }
 
     public function delay(float $delay, Closure $callback)
@@ -32,7 +46,20 @@ class SwooleScheduler implements SchedulerInterface
         if ($delay <= 0) {
             $delay = 1;
         }
-        swoole_timer_after($delay, $callback);
+        $timer = swoole_timer_after($delay, $callback);
+
+        return new class($timer) extends Timer {
+            private $timer;
+
+            public function __construct($timer) {
+                $this->timer = $timer;
+            }
+            public function run() {}
+            public function stop() {
+                swoole_timer_clear($this->timer);
+                parent::stop();
+            }
+        };
     }
 
     public function io($resource, ?Closure $callback)
