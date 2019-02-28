0b09b9d00cdf1900910d96e052713d4e07a173f9
Dimitar Dimitrov <daghostman.dd@gmail.com>
2019-02-28 11:01:11 +0200

    Modified server example

--- examples/server.php
+++ examples/server.php
@@ -1,11 +1,12 @@
 <?php
 
+use function Onion\Framework\EventLoop\coroutine;
+use function Onion\Framework\EventLoop\defer;
 use function Onion\Framework\EventLoop\io;
 use function Onion\Framework\EventLoop\loop;
+use function Onion\Framework\EventLoop\scheduler;
 use function Onion\Framework\EventLoop\timer;
 use Onion\Framework\EventLoop\Stream\Stream;
-use function Onion\Framework\EventLoop\scheduler;
-use function Onion\Framework\EventLoop\coroutine;
 require __DIR__ . '/../vendor/autoload.php';
 
 ini_set('display_errors', 1);
@@ -17,19 +18,25 @@ $scheduler = scheduler();
 if (!$socket) throw new Exception($errStr, $errNo);
 echo "Starting server at port $port...\n";
 timer(0.0, function () use ($socket) {
+    if (!is_resource($socket)) {
+        return;
+    }
+
     stream_set_blocking($socket, 0);
     $channel = @stream_socket_accept($socket, 0);
 
-    if ($channel) {
-        loop(true);
+    if (!is_resource($channel)) {
+        usleep(1);
+        return;
+    }
 
-        io($socket, function (Stream $stream) use ($channel) {
-            $data = $stream->read();
+    loop(true);
+    io($channel, function (Stream $stream) {
+        $data = $stream->read();
 
-            return io($channel, null, function (Stream $stream) use ($data) {
-                $msg = "Received following request:\n\n$data";
-                $msgLength = strlen($msg);
-                $response = <<<RES
+        $msg = "Received following request:\n\n$data";
+        $msgLength = strlen($msg);
+        $response = <<<RES
 HTTP/1.1 200 OK\r
 Content-Type: text/plain\r
 Content-Length: $msgLength\r
@@ -38,17 +45,11 @@ Connection: close\r
 $msg
 RES;
 
-                $stream->write($response);
-                $stream->close();
-            });
-        });
+        $stream->write($response);
+        $stream->close();
+    });
 
-        coroutine(function () {
-            echo "Test\n";
-        });
-
-        loop()->start();
-    }
+    loop()->start();
 });
 
 loop()->start();
