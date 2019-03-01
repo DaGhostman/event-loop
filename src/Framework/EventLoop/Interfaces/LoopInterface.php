e2f9fa6f49a25fd58c47ae7d976b44d488583cdb
Dimitar Dimitrov <daghostman.dd@gmail.com>
2019-03-01 14:23:51 +0200

    Remove `kill` method

--- src/Framework/EventLoop/Interfaces/LoopInterface.php
+++ src/Framework/EventLoop/Interfaces/LoopInterface.php
@@ -1,13 +1,17 @@
 <?php
 namespace Onion\Framework\EventLoop\Interfaces;
 
+use Closure;
+
 interface LoopInterface
 {
     const TASK_IMMEDIATE = 0;
     const TASK_DEFERRED = 1;
 
+    public function attach($resource, ?Closure $onRead = null, ?Closure $onWrite = null): bool;
+    public function detach($resource): bool;
+
     public function start(): void;
     public function push(TaskInterface $task, int $type = self::TASK_IMMEDIATE): TaskInterface;
     public function stop(): void;
-    public function kill(): void;
 }
