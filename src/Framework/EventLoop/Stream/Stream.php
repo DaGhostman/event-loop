f7f9046bc365edfa9c86e172f550e77e0c8569d4
Dimitar Dimitrov <daghostman.dd@gmail.com>
2019-03-01 14:26:31 +0200

    Remove useless methods

--- src/Framework/EventLoop/Stream/Stream.php
+++ src/Framework/EventLoop/Stream/Stream.php
@@ -3,71 +3,29 @@ namespace Onion\Framework\EventLoop\Stream;
 
 class Stream
 {
-    private const READABLE_MODES = [
-        'r',
-        'rb',
-        'rt',
-        'r+',
-        'r+b',
-        'r+t',
-        'w+',
-        'w+b',
-        'w+t',
-        'a+',
-        'a+b',
-        'a+t',
-        'x+',
-        'x+b',
-        'x+t',
-        'c+',
-        'c+b',
-        'c+t',
-    ];
-
-    private const WRITABLE_MODES = [
-        'r+',
-        'r+b',
-        'r+t',
-        'w',
-        'wb',
-        'w+',
-        'w+b',
-        'w+t',
-        'a',
-        'ab',
-        'a+',
-        'a+b',
-        'x',
-        'xb',
-        'x+',
-        'x+b',
-        'x+t',
-        'c',
-        'cb',
-        'c+',
-        'c+b',
-        'c+t',
-    ];
-
     private $resource;
-    private $readable;
-    private $writable;
 
-    public function __construct($resource, bool $readable = false, bool $writable = false)
+    public function __construct($resource)
     {
         $this->resource = $resource;
-        $this->readable = $readable;
-        $this->writable = $writable;
     }
 
-    public function read(int $size = 8192): string
+    public function line(): ?string
     {
-        return @fread($this->resource, $size);
+        $result = @fgets($this->resource, $size);
+        return $result !== false ? $result : null;
     }
 
-    public function write(string $contents): int
+    public function read(int $size = 8192): ?string
     {
-        return (int) @fwrite($this->resource, $contents);
+        $result = @fread($this->resource, $size);
+        return $result !== false ? $result : null;
+    }
+
+    public function write(string $contents): ?int
+    {
+        $result = @fputs($this->resource, $contents);
+        return $result !== false ? $result : null;
     }
 
     public function eof(): bool
@@ -78,11 +36,6 @@ class Stream
     public function tell(): int
     {
         return ftell($this->resource);
-    }
-
-    public function lock(int $lock = LOCK_EX)
-    {
-        return @flock($this->resource, $lock);
     }
 
     public function close(): bool
@@ -119,22 +72,6 @@ class Stream
         return stream_is_local($this->resource);
     }
 
-    public function isReadable()
-    {
-        $mode = stream_get_meta_data($this->resource)['mode'] ?? '';
-        $readable = in_array($mode, self::READABLE_MODES, true);
-
-        return $readable && $this->readable;
-    }
-
-    public function isWritable()
-    {
-        $mode = stream_get_meta_data($this->resource)['mode'] ?? '';
-        $writable = in_array($mode, self::WRITABLE_MODES, true);
-
-        return $writable && $this->writable;
-    }
-
     public function detach()
     {
         $this->readable = $this->writable = false;
@@ -147,10 +84,5 @@ class Stream
     public function tty(): bool
     {
         return stream_isatty($this->resource);
-    }
-
-    public function __destruct()
-    {
-        $this->close();
     }
 }
