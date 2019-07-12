dd2b75e5594d1dfd4d2384d0d327dc72f8a045d3
Dimitar Dimitrov <daghostman.dd@gmail.com>
2019-07-12 14:36:25 +0300

    update `accpet` to return Signal

--- src/Loop/Interfaces/SocketInterface.php
+++ src/Loop/Interfaces/SocketInterface.php
@@ -2,8 +2,9 @@
 namespace Onion\Framework\Loop\Interfaces;
 
 use Onion\Framework\Loop\Interfaces\ResourceInterface;
+use Onion\Framework\Loop\Signal;
 
 interface SocketInterface extends ResourceInterface
 {
-    public function accept(): ResourceInterface;
+    public function accept(): Signal;
 }
--- src/Loop/Socket.php
+++ src/Loop/Socket.php
@@ -1,9 +1,12 @@
 <?php
+
 namespace Onion\Framework\Loop;
 
 use Onion\Framework\Loop\Descriptor;
-use Onion\Framework\Loop\Interfaces\ResourceInterface;
 use Onion\Framework\Loop\Interfaces\SocketInterface;
+use Onion\Framework\Loop\Interfaces\TaskInterface;
+use Onion\Framework\Loop\Interfaces\SchedulerInterface;
+use Onion\Framework\Loop\Interfaces\ResourceInterface;
 
 class Socket extends Descriptor implements SocketInterface
 {
@@ -26,8 +29,15 @@ class Socket extends Descriptor implements SocketInterface
         );
     }
 
-    public function accept(?int $timeout = 0): ResourceInterface
+    public function accept(?int $timeout = 0): Signal
     {
-        return new Descriptor(@stream_socket_accept($this->getDescriptor(), $timeout));
+        $waitFn = function (TaskInterface $task, SchedulerInterface $scheduler, ResourceInterface $resource, ?int $timeout) {
+            $task->send(new Descriptor(@stream_socket_accept($resource->getDescriptor(), $timeout)));
+            yield $scheduler->schedule($task);
+        };
+
+        return new Signal(function (TaskInterface $task, SchedulerInterface $scheduler) use ($timeout, $waitFn) {
+            $scheduler->add(new Coroutine($waitFn, [$task, $scheduler, $this, $timeout]));
+        });
     }
 }
