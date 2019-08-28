9d3c93fc0e760bbf2f90bf4ae7a191d9ae1148b9
Dimitar Dimitrov <daghostman.dd@gmail.com>
2019-08-28 14:19:53 +0300

    update example to demonstrate error handling

--- examples/process.php
+++ examples/process.php
@@ -1,15 +1,19 @@
 <?php
 
-use Onion\Framework\Loop\Coroutine;
-use Onion\Framework\Process\Process;
-
 use function Onion\Framework\Loop\scheduler;
+use Onion\Framework\Loop\Coroutine;
+
+use Onion\Framework\Process\Process;
 
 require __DIR__ . '/../vendor/autoload.php';
 
 scheduler()->add(new Coroutine(function () {
     $proc = Process::exec('composer', ['info']);
     $proc->unblock();
+    $proc->onError(function ($s) {
+        echo "Error: " . $s->read(8192);
+    });
+    echo "Started process: {$proc->getPid()}\n";
 
     while ($proc->isAlive()) {
         yield $proc->wait();
@@ -19,6 +23,7 @@ scheduler()->add(new Coroutine(function () {
         }
         echo PHP_EOL;
     }
+
     echo "Exited with: {$proc->getExitCode()}\n";
 }));
 scheduler()->start();
