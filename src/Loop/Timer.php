8bfe8aeb22ce4341687d8103e59852482f482e06
Dimitar Dimitrov <daghostman.dd@gmail.com>
2022-12-02 17:57:46 +0200

    fix repeated timer handling

--- src/Loop/Timer.php
+++ src/Loop/Timer.php
@@ -24,12 +24,26 @@ class Timer implements TimerInterface
     {
         // Convert milliseconds to microseconds
         $ms *= 1000;
-        $task = Task::create(static function (Closure $coroutine, int $ms, bool $repeating) {
-            coroutine($coroutine);
-            if ($repeating) {
-                scheduler()->schedule(Coroutine::task(), (int) floor(hrtime(true) / 1e+3) + $ms);
-            }
-        }, [$coroutine, $ms, $repeating]);
+        $task = Task::create(
+            static function (Closure $coroutine, int $ms, bool $repeating) use (&$tick) {
+                $tick = ((int) floor(hrtime(true) / 1e+3));
+                coroutine($coroutine);
+
+                if ($repeating) {
+                    while (true) {
+                        signal(
+                            fn ($resume, $task, $scheduler) => $scheduler->schedule(
+                                Task::create($resume),
+                                $tick + $ms,
+                            )
+                        );
+                        $tick = ((int) floor(hrtime(true) / 1e+3));
+                        coroutine($coroutine);
+                    }
+                }
+            },
+            [$coroutine, $ms, $repeating]
+        );
 
         scheduler()->schedule($task, ((int) floor(hrtime(true) / 1e+3)) + $ms);
 
