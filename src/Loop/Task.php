d5a2cf8ac46118f45f20e8519790a71e2e37544b
Dimitar Dimitrov <daghostman.dd@gmail.com>
2023-02-07 15:41:22 +0200

    update task handling of fibers and deferreds

--- src/Loop/Task.php
+++ src/Loop/Task.php
@@ -2,26 +2,59 @@
 
 namespace Onion\Framework\Loop;
 
+use Closure;
 use Fiber;
-use Onion\Framework\Loop\Interfaces\CoroutineInterface;
 use Onion\Framework\Loop\Interfaces\TaskInterface;
+use Onion\Framework\Promise\Deferred;
+use Onion\Framework\Promise\Interfaces\DeferredInterface;
+use Spatie\Backtrace\Backtrace;
+use Throwable;
 
 class Task implements TaskInterface
 {
-    protected bool $killed = false;
+    private ?Throwable $exception = null;
+    private mixed $value = null;
 
-    public function __construct(private readonly CoroutineInterface $coroutine)
-    {
+    protected bool $killed = false;
+    public readonly Backtrace $trace;
+
+    private ?DeferredInterface $deferred = null;
+
+    public function __construct(
+        private readonly Fiber $coroutine,
+        private readonly mixed $args,
+    ) {
+        $this->trace = Backtrace::create();
     }
 
     public function run(): mixed
     {
-        return $this->coroutine->run();
+        if ($this->exception) {
+            return $this->coroutine->throw($this->exception);
+        }
+
+        try {
+            if (!$this->coroutine->isStarted()) {
+                $result = $this->coroutine->start(...$this->args);
+            } else {
+                $result = $this->coroutine->resume($this->value);
+            }
+
+            if ($this->exception === null && $this->coroutine->isTerminated()) {
+                $this->deferred?->resolve($this->coroutine->getReturn());
+            }
+
+            return $result;
+        } catch (Throwable $ex) {
+            $this->deferred?->reject($ex);
+
+            throw $ex;
+        }
     }
 
     public function suspend(mixed $value = null): void
     {
-        if (!$this->coroutine->isPaused()) {
+        if (!$this->coroutine->isSuspended()) {
             $this->coroutine->suspend($value);
         }
     }
@@ -32,18 +65,18 @@ class Task implements TaskInterface
             return false;
         }
 
-        $this->coroutine->send($value);
+        $this->value = $value;
 
         return true;
     }
 
-    public function throw(\Throwable $exception): bool
+    public function throw(Throwable $exception): bool
     {
         if ($this->isKilled()) {
             return false;
         }
 
-        $this->coroutine->throw($exception);
+        $this->exception = $exception;
 
         return true;
     }
@@ -65,11 +98,36 @@ class Task implements TaskInterface
 
     public function isPaused(): bool
     {
-        return $this->coroutine->isRunning() && $this->coroutine->isPaused();
+        return $this->coroutine->isStarted() && $this->coroutine->isSuspended();
+    }
+
+    public function deferred(): DeferredInterface
+    {
+        if (!isset($this->deferred)) {
+            if ($this->coroutine->isTerminated()) {
+                throw new \LogicException('Unable to create a promise for completed task');
+            }
+
+            $this->deferred = new Deferred();
+        }
+
+        return $this->deferred;
+    }
+
+    public static function defer(): DeferredInterface
+    {
+        return signal(function (Closure $resume, TaskInterface $task) {
+            $resume($task->deferred());
+        });
+    }
+
+    public function sync(): mixed
+    {
+        return \Onion\Framework\Promise\await($this->defer()->promise());
     }
 
     public static function create(callable $fn, array $args = []): TaskInterface
     {
-        return new Task(new Coroutine(new Fiber($fn), $args));
+        return new Task(new Fiber($fn), $args);
     }
 }
